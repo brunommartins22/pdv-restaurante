8a906e122810570705ce828c8de0b719d792d161
dinael <tadsdinael@gmail.com>
2019-11-13 13:10:24 -0300

    padrao

--- src/app/pages/page-fornecedor/page-fornecedor.component.ts
+++ src/app/pages/page-fornecedor/page-fornecedor.component.ts
@@ -8,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageFornecedorComponent extends CrudComponent {
 
+    pessoafisica = [
+        { label: 'Pessoa Fisica', value: 'FISICA' },
+        { label: 'Pessoa Juridica', value: 'JURIDICA' },
 
+    ];
+
+    instance() {
+        this.objetoSelecionado = new Object();
+        this.objetoSelecionado.pessoa = new Object();
+    }
 
     ngOnInit() {
 
         document.body.classList.remove('body-img');
-
         super.iniciar('/fornecedores');
+
+
+
+    }
+
+    limpaCamposTipoPessoa() {
+        this.objetoSelecionado.cnpjCpf = null;
+        this.objetoSelecionado.ieRg = null;
     }
 
 }
