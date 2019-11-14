f0d86c29f37c2543449795e1d8d44151af854dcd
dinael <tadsdinael@gmail.com>
2019-11-14 12:45:11 -0300

    Padrao

--- src/app/pages/page-fornecedor/page-fornecedor.component.ts
+++ src/app/pages/page-fornecedor/page-fornecedor.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageFornecedorComponent extends CrudComponent {
 
+    listEndereco: any;
+    display: boolean = true;
+    item: any;
+    endereco: boolean;
     pessoafisica = [
         { label: 'Pessoa Fisica', value: 'FISICA' },
         { label: 'Pessoa Juridica', value: 'JURIDICA' },
@@ -24,13 +28,40 @@ export class PageFornecedorComponent extends CrudComponent {
         document.body.classList.remove('body-img');
         super.iniciar('/fornecedores');
 
+        this.cols = [
+            { field: 'nomePessoa', header: 'Razão Social', width: '70px' },
+            { field: 'nomeFantasia', header: 'Logradouro', width: '70px' },
+            { field: 'cnpjCpf', header: 'CNPJ / CPF', width: '30px' },
+            { field: 'ieRg', header: 'IE / RG', width: '20px' },
+            { field: 'email', header: 'Email', width: '30px' },
+
+
+            // { field: 'endereco', subfild: ' logradouro', header: 'Logradouro', width: '70px' },
+            // { field: 'endereco', subfild: 'cep', header: 'Cep', width: '10px' },
+            // { field: 'endereco', subfild: 'bairro', header: 'Bairro', width: '10px' },
+            // { field: 'endereco', subfild: 'complemento', header: 'Complemento', width: '30px' },
+            // { field: 'endereco', subfild: 'numero', header: 'numero', width: '10px' },
+            // { field: 'endereco', subfild: 'cidade', header: 'cidade', width: '20px' },
+            // { field: 'endereco', subfild: 'estado', header: 'estado', width: '10px' }
+        ];
+
 
 
     }
+
+    getListEndereco() {
+        this.httpUtilService.get(this.urlControler + '/pessoa').subscribe(data => {
+            this.listEndereco = data.json;
+        });
+    }
+
+
 
     limpaCamposTipoPessoa() {
         this.objetoSelecionado.cnpjCpf = null;
         this.objetoSelecionado.ieRg = null;
     }
+
+
 
 }
